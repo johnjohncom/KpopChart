8944d54ec3ab9e4b51459577d90ee6888863ca47
son7211 <son7211@github.com>
2025-06-27 12:31:06 +0900

    라우팅 및 기본 페이지, Header 컴포넌트, React Router 적용

---
+++ src/pages/BugsChart.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const BugsChart = () => <div>벅스 탑100 차트 페이지</div>;
+
+export default BugsChart;
---
+++ src/pages/GenieChart.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const GenieChart = () => <div>지니 탑200 차트 페이지</div>;
+
+export default GenieChart;
